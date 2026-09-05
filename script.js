28363e069fbff684ba35c24fba308a48b341dc45
JoshWestlake <jl_westlake@outlook.com>
2026-09-05 17:03:48 -0400

    Major Update
    yeppers

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function toggleMenu2(){
 }
 
 /*toggles the mode of the menu/hamburger which updates in html file -> causes the dropdown*/
+
+/*toggles the mobile dropdown for the site-wide nav bar*/
+function toggleSiteNav(){
+    const links = document.getElementById("siteNavLinks");
+    if (links) links.classList.toggle("open");
+}
+
